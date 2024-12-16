54cd07555a960f031e9ea2aa7da2b000919b8be5
alinebmr <alinebrito.maracaja@gmail.com>
2024-12-16 17:42:37 -0300

    correcoes .env e application properties

--- Adptare-master/adptare/src/main/java/com/ufcg/adptare/AdptareApplication.java
+++ Adptare-master/adptare/src/main/java/com/ufcg/adptare/AdptareApplication.java
@@ -2,11 +2,22 @@ package com.ufcg.adptare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import me.paulschwarz.springdotenv.DotenvPropertySource;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import me.paulschwarz.springdotenv.DotenvPropertySource;
 
 @SpringBootApplication
 public class AdptareApplication {
 
 	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+    	// Add DotenvPropertySource to environment before registering components
+    	DotenvPropertySource.addToEnvironment(applicationContext.getEnvironment());
 		SpringApplication.run(AdptareApplication.class, args);
 	}
 
